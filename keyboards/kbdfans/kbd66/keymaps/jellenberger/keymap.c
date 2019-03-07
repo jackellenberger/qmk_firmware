df8f9aaa479cbdc9976a77dd925ef3886c303569
jackellenberger <jellenberger@uchicago.edu>
2019-03-07 10:03:48 -0600

    fixup kbd66:jellenberger

--- keyboards/kbdfans/kbd66/keymaps/jellenberger/keymap.c
+++ keyboards/kbdfans/kbd66/keymaps/jellenberger/keymap.c
@@ -28,19 +28,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|   |---|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|   |DEL|
      * |-----------------------------------------------------------|   `---'
-     * |Cap  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Enter  |
+     * |CTL  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Enter  |
      * |---------------------------------------------------------------.
      * |Shft|  `|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| Shft |Fn | UP|
      * |-------------------------------------------------------------------.
-     * |Ctl |OS  |Alt |         Space           |Alt | Fn |Ctl |LFT|DWN|RIG|
+     * | FN |OS  |Alt |         Space           |Alt | OS |Ctl |LFT|DWN|RIG|
      * `-------------------------------------------------------------------'
      */
 [_L0] = LAYOUT_all(
   KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_INS, KC_BSPC,            KC_GRV,
   KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,   KC_BSLS,               KC_DEL,
-  KC_CAPS,     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,    KC_ENT,
+  KC_LCTL,     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,    KC_ENT,
   KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_L1), KC_UP,
-  KC_LCTL, KC_LGUI, KC_LALT,   KC_SPC, KC_SPC,               KC_RALT, MO(_L1), KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+  MO(_L1), KC_LGUI, KC_LALT,   KC_SPC, KC_SPC,               KC_RALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
     /* Layer 1
